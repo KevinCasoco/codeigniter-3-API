31c1845d22753d752519093253e81d633d8f6bfc
KevinCasoco <andreikevincasoco@gmail.com>
2024-06-24 20:13:34 +0800

    REST API test and fixed error

---
+++ application/controllers/api/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/RestController.php';
+
+use chriskacerguis\RestServer\RestController;
+
+
+class ApiController extends RestController 
+{
+	public function index_get()
+	{
+		echo "This is restful API test";
+	}
+
+}
+
+?>
